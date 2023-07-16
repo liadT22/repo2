c036e6285b916570c9718f6d86dd91a31eb6cfa7
liadT22 <liadt22@gmail.com>
2023-07-16 20:31:43 +0300

    Changed LiadTiblum so it will print Liad Tiblum

--- LiadTiblum.java
+++ LiadTiblum.java
@@ -1,7 +1,7 @@
 public class LiadTiblum{
     public static void main(String[] args){
         for (int i = 1; i<=10; i++){
-            System.out.println("My name should have been printed here");
+            System.out.println("Liad Tiblum");
         }
     }
 }
